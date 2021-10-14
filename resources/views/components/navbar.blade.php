cd8b1430dda41e96a814faf0cbbbb67849f9f7e4
vimarteleto <viniciusmarteleto@gmail.com>
2021-10-13 23:15:26 -0300

    estilizacao da navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,18 +1,24 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
+    
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
                 aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
-        {{-- <div class="collapse navbar-collapse" id="navbar"> --}}
+        <a href="/">
+            <img src="{{asset('img/g.jpg')}}" alt="" srcset="" height="45px" style="border-radius: 0.25rem">
+        </a>
+        <div style="color: white">
+           Sistema de Gestão Industrial
+        </div>
+        {{-- <div class="collapse navbar-collapse" id="navbar">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link {{$current == "home" ? 'active' : ''}}"
-                       aria-current="page" href="/">Home</a>
+                    
+
                 </li>
 
-                {{-- <li class="nav-item">
+                <li class="nav-item">
                     <a class="nav-link {{$current == "categorias" ? 'active' : ''}}"
                        aria-current="page" href="/categorias">Categorias</a>
                 </li>
@@ -20,10 +26,11 @@
                 <li class="nav-item">
                     <a class="nav-link {{$current == "materiais" ? 'active' : ''}}"
                        aria-current="page" href="/materiais">Materiais</a>
-                </li> --}}
+                </li>
 
             </ul>
 
-        {{-- </div> --}}
+        </div> --}}
     </div>
+    
 </nav>
